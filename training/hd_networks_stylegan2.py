98d535f5a20e5ca35ae4f76e88e640110ab72a06
zhuxinqimac <zhuxinqimac@yahoo.com>
2020-04-23 23:32:03 +1000

    add hyperdir constraint2

--- training/hd_networks_stylegan2.py
+++ training/hd_networks_stylegan2.py
@@ -8,7 +8,7 @@
 
 # --- File Name: hd_networks_stylegan2.py
 # --- Creation Date: 22-04-2020
-# --- Last Modified: Thu 23 Apr 2020 20:08:13 AEST
+# --- Last Modified: Thu 23 Apr 2020 23:31:39 AEST
 # --- Author: Xinqi Zhu
 # .<.<.<.<.<.<.<.<.<.<.<.<.<.<.<.<
 """
@@ -176,12 +176,14 @@ def G_mapping_hd_dis_to_dlatent(
             else:
                 start_tmp = end_tmp
                 end_tmp = end_tmp + seg
-            with tf.variable_scope('Bottleneck'):
-                tmp = networks_stylegan2.apply_bias_act(networks_stylegan2.dense_layer(x[:, start_tmp:end_tmp], fmaps=mapping_fmaps, lrmul=mapping_lrmul),
-                                     act=act, lrmul=mapping_lrmul)
+            # with tf.variable_scope('Bottleneck'):
+                # tmp = networks_stylegan2.apply_bias_act(networks_stylegan2.dense_layer(x[:, start_tmp:end_tmp], fmaps=mapping_fmaps, lrmul=mapping_lrmul),
+                                     # act=act, lrmul=mapping_lrmul)
+            # with tf.variable_scope('to_dlatent'):
+                # x_list.append(networks_stylegan2.apply_bias_act(networks_stylegan2.dense_layer(tmp, fmaps=dlatent_size, lrmul=mapping_lrmul),
+                                             # act=act, lrmul=mapping_lrmul))
             with tf.variable_scope('to_dlatent'):
-                x_list.append(networks_stylegan2.apply_bias_act(networks_stylegan2.dense_layer(tmp, fmaps=dlatent_size, lrmul=mapping_lrmul),
-                                             act=act, lrmul=mapping_lrmul))
+                x_list.append(x[:, start_tmp:end_tmp])
     x = tf.stack(x_list, axis=1)
 
     # # Broadcast.
