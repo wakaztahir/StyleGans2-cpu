58f41a93e0de4685dbfa8d63f9d3e9d7e2ea01c2
zhuxinqimac <zhuxinqimac@yahoo.com>
2020-04-23 18:02:13 +1000

    add hyperdir constraint2

--- training/hd_networks_stylegan2.py
+++ training/hd_networks_stylegan2.py
@@ -8,7 +8,7 @@
 
 # --- File Name: hd_networks_stylegan2.py
 # --- Creation Date: 22-04-2020
-# --- Last Modified: Wed 22 Apr 2020 23:47:33 AEST
+# --- Last Modified: Thu 23 Apr 2020 18:01:13 AEST
 # --- Author: Xinqi Zhu
 # .<.<.<.<.<.<.<.<.<.<.<.<.<.<.<.<
 """
@@ -17,8 +17,9 @@ StyleGAN2-like networks used in HD models.
 
 import numpy as np
 import tensorflow as tf
-from training.networks_stylegan2 import dense_layer
-from training.networks_stylegan2 import apply_bias_act
+# from training.networks_stylegan2 import dense_layer
+# from training.networks_stylegan2 import apply_bias_act
+import training.networks_stylegan2
 
 #----------------------------------------------------------------------------
 # G_mapping with hd_dis network.
@@ -57,7 +58,7 @@ def G_mapping_hd_dis_to_dlatent(
     # for layer_idx in range(mapping_layers):
         # with tf.variable_scope('Dense%d' % layer_idx):
             # fmaps = dlatent_size if layer_idx == mapping_layers - 1 else mapping_fmaps
-            # x = apply_bias_act(dense_layer(x, fmaps=fmaps, lrmul=mapping_lrmul), act=act, lrmul=mapping_lrmul)
+            # x = networks_stylegan2.apply_bias_act(networks_stylegan2.dense_layer(x, fmaps=fmaps, lrmul=mapping_lrmul), act=act, lrmul=mapping_lrmul)
 
 
     x_list = []
@@ -73,10 +74,10 @@ def G_mapping_hd_dis_to_dlatent(
                 start_tmp = end_tmp
                 end_tmp = end_tmp + seg
             with tf.variable_scope('Bottleneck'):
-                tmp = apply_bias_act(dense_layer(x[:, start_tmp:end_tmp], fmaps=mapping_fmaps, lrmul=mapping_lrmul),
+                tmp = networks_stylegan2.apply_bias_act(networks_stylegan2.dense_layer(x[:, start_tmp:end_tmp], fmaps=mapping_fmaps, lrmul=mapping_lrmul),
                                      act=act, lrmul=mapping_lrmul)
             with tf.variable_scope('to_dlatent'):
-                x_list.append(apply_bias_act(dense_layer(tmp, fmaps=dlatent_size, lrmul=mapping_lrmul),
+                x_list.append(networks_stylegan2.apply_bias_act(networks_stylegan2.dense_layer(tmp, fmaps=dlatent_size, lrmul=mapping_lrmul),
                                              act=act, lrmul=mapping_lrmul))
     x = tf.stack(x_list, axis=1)
 
