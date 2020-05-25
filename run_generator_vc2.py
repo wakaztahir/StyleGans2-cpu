8fdc88efbb07192929206e6a1e7faad69b19e4e5
zhuxinqimac <zhuxinqimac@yahoo.com>
2020-05-26 03:59:20 +1000

    generate

--- run_generator_vc2.py
+++ run_generator_vc2.py
@@ -8,7 +8,7 @@
 
 # --- File Name: run_generator_vc2.py
 # --- Creation Date: 26-05-2020
-# --- Last Modified: Tue 26 May 2020 03:55:22 AEST
+# --- Last Modified: Tue 26 May 2020 03:59:13 AEST
 # --- Author: Xinqi Zhu
 # .<.<.<.<.<.<.<.<.<.<.<.<.<.<.<.<
 """
@@ -39,7 +39,7 @@ def generate_images(network_pkl, seeds, create_new_G, new_func_name):
 
     Gs_kwargs = dnnlib.EasyDict()
     # Gs_kwargs.output_transform = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
-    Gs_kwargs.randomize_noise = False
+    Gs_kwargs.randomize_noise = True
 
     for seed_idx, seed in enumerate(seeds):
         print('Generating image for seed %d (%d/%d) ...' % (seed, seed_idx, len(seeds)))
