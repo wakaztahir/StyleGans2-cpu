c8f14edce66d4a48754bd252309d355ad4404a2e
zhuxinqimac <zhuxinqimac@yahoo.com>
2020-05-26 04:07:24 +1000

    generate

--- run_generator_vc2.py
+++ run_generator_vc2.py
@@ -8,7 +8,7 @@
 
 # --- File Name: run_generator_vc2.py
 # --- Creation Date: 26-05-2020
-# --- Last Modified: Tue 26 May 2020 03:59:13 AEST
+# --- Last Modified: Tue 26 May 2020 04:07:15 AEST
 # --- Author: Xinqi Zhu
 # .<.<.<.<.<.<.<.<.<.<.<.<.<.<.<.<
 """
@@ -35,7 +35,7 @@ def generate_images(network_pkl, seeds, create_new_G, new_func_name):
     _G, _D, I, Gs = misc.load_pkl(network_pkl)
     if create_new_G:
         Gs = Gs.convert(new_func_name=new_func_name)
-    noise_vars = [var for name, var in Gs.components.synthesis.vars.items() if name.startswith('noise')]
+    # noise_vars = [var for name, var in Gs.components.synthesis.vars.items() if name.startswith('noise')]
 
     Gs_kwargs = dnnlib.EasyDict()
     # Gs_kwargs.output_transform = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
@@ -45,7 +45,7 @@ def generate_images(network_pkl, seeds, create_new_G, new_func_name):
         print('Generating image for seed %d (%d/%d) ...' % (seed, seed_idx, len(seeds)))
         rnd = np.random.RandomState(seed)
         z = rnd.randn(1, *Gs.input_shape[1:]) # [minibatch, component]
-        tflib.set_vars({var: rnd.randn(*var.shape.as_list()) for var in noise_vars}) # [height, width]
+        # tflib.set_vars({var: rnd.randn(*var.shape.as_list()) for var in noise_vars}) # [height, width]
         images, _ = Gs.run(z, None, **Gs_kwargs) # [minibatch, height, width, channel]
         images = misc.adjust_dynamic_range(images, [-1, 1], [0, 255])
         images = np.transpose(images, [0, 2, 3, 1]).astype(np.uint8)
