b35af76a046b06b4278a8892c2c6ea6f4fc02b9d
zhuxinqimac <zhuxinqimac@yahoo.com>
2020-08-11 17:37:03 +1000

    proj v2

--- projector_vc2.py
+++ projector_vc2.py
@@ -8,7 +8,7 @@
 
 # --- File Name: projector_vc2.py
 # --- Creation Date: 23-05-2020
-# --- Last Modified: Tue 11 Aug 2020 17:30:03 AEST
+# --- Last Modified: Tue 11 Aug 2020 17:36:59 AEST
 # --- Author: Xinqi Zhu
 # .<.<.<.<.<.<.<.<.<.<.<.<.<.<.<.<
 """
@@ -42,7 +42,7 @@ class ProjectorVC2:
         self.lr_rampup_length           = 0.05
         self.noise_ramp_length          = 0.75
         # self.regularize_noise_weight    = 1e5
-        self.regularize_noise_weight    = 1e8
+        self.regularize_noise_weight    = 1e9
         self.verbose                    = False
         self.clone_net                  = True
 
