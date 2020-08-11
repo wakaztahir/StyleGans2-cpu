65efda30a4fd1e2b7aa45805623950eec14a8644
zhuxinqimac <zhuxinqimac@yahoo.com>
2020-08-11 17:47:18 +1000

    proj v2

--- projector_vc2.py
+++ projector_vc2.py
@@ -8,7 +8,7 @@
 
 # --- File Name: projector_vc2.py
 # --- Creation Date: 23-05-2020
-# --- Last Modified: Tue 11 Aug 2020 17:42:11 AEST
+# --- Last Modified: Tue 11 Aug 2020 17:47:14 AEST
 # --- Author: Xinqi Zhu
 # .<.<.<.<.<.<.<.<.<.<.<.<.<.<.<.<
 """
@@ -38,7 +38,7 @@ class ProjectorVC2:
         self.num_steps                  = 2000
         self.dlatent_avg_samples        = 10000
         # self.initial_learning_rate      = 0.1
-        self.initial_learning_rate      = 1
+        self.initial_learning_rate      = 0.2
         self.initial_noise_factor       = 0.05
         self.lr_rampdown_length         = 0.25
         self.lr_rampup_length           = 0.05
